23498a39dc455134982a5ea11168ef4879d19c4c
Wei Jie <weijie_1993@msn.com>
2024-07-14 18:01:30 +0800

    Added Green Outline when selected

--- Final Capstone/GUI/build/controller_db.py
+++ Final Capstone/GUI/build/controller_db.py
@@ -2,19 +2,28 @@ import pandas as pd
 from collections import defaultdict, namedtuple
 
 DB_FILE = "C:/Users/USER/PycharmProjects/MtechISS/Final Capstone/GUI/build/assets/DB/DB File.xlsx"
-
+DB_CONFIG = 'C:/Users/USER/PycharmProjects/MtechISS/Final Capstone/GUI/build/assets/DB/Department Short Codes.xlsx'
 
 def refresh_database():
     global main_stats_month
     main_stats_month = pd.read_excel(DB_FILE, sheet_name='Month_Stats')
     return main_stats_month
 
-def get_all_months_data(year=2024, month=1):
-    filtered = main_stats_month[main_stats_month.Months.str.contains(str(year))]
+def refresh_config():
+    global department_config
+    department_config = pd.read_excel(DB_FILE, sheet_name='Department_config')
+    return department_config
+
+def month2idx(months="Jul"):
+    MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    return MONTHS.index(months) + 1
+def get_all_months_data(year:int=2024, month:int=1, department='ALL'):
+    department_data = main_stats_month[main_stats_month.Department==department]
+    filtered = department_data[department_data.Months.str.contains(str(year))]
     MonthData = namedtuple(typename='MonthData', field_names=['month_txt', 'per_txt', 'rec_colour'])
     MONTH_DATA = {'Year': str(year)}
     MONTHS = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-    for i in range(12):
+    for i in range(1,13):
         output = filtered.query(f"Months == '{year}-{str(i).zfill(2)}'")
         if len(output) == 0:
             percentage = 0
@@ -32,9 +41,9 @@ def get_all_months_data(year=2024, month=1):
             else:
                 return 'green'
         color = get_colour(percentage)
-        MONTH_DATA[MONTHS[i]] = MonthData(MONTHS[i], f'{percentage}%', color)
-    month = month - 1
-    filtered_months = main_stats_month[main_stats_month.Months == f"{str(year)}-{str(month).zfill(2)}"]
+        MONTH_DATA[MONTHS[i-1]] = MonthData(MONTHS[i-1], f'{percentage}%', color)
+
+    filtered_months = department_data[department_data.Months == f"{str(year)}-{str(month).zfill(2)}"]
     if len(filtered_months) == 0:
         statistics = {f'{MONTHS[month]} WTA': '-',
                   f'{MONTHS[month]} Demand': '-',
@@ -46,12 +55,12 @@ def get_all_months_data(year=2024, month=1):
 
     else:
         filtered_months = filtered_months.iloc[0].fillna(0)
-        statistics = {f'{MONTHS[month]} WTA': f'{filtered_months.Per_WTA}%',
-                  f'{MONTHS[month]} Demand': f'{filtered_months.Demand}',
-                  f'{MONTHS[month]} Supply': f'{filtered_months.Supply}',
-                  f'{MONTHS[month]} Predicted WTA': '30%',
-                  f'{MONTHS[month]} Total Expected': f'{filtered_months.Expected}',
-                  f'{MONTHS[month]} Supply Adjustment': '130'
+        statistics = {f'{MONTHS[month-1]} WTA': f'{filtered_months.Per_WTA}%',
+                  f'{MONTHS[month-1]} Demand': f'{filtered_months.Demand}',
+                  f'{MONTHS[month-1]} Supply': f'{filtered_months.Supply}',
+                  f'{MONTHS[month-1]} Predicted WTA': '30%',
+                  f'{MONTHS[month-1]} Total Expected': f'{filtered_months.Expected}',
+                  f'{MONTHS[month-1]} Supply Adjustment': '130'
                   }
 
 
---
+++ Final Capstone/GUI/build/main_gui.py
@@ -0,0 +1,225 @@
+import tkinter
+from datetime import datetime
+
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+from main_util import *
+from collections import namedtuple
+from controller_db import month2idx
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = Path(r"C:\Users\USER\PycharmProjects\MtechISS\Final Capstone\GUI\build\assets\frame0")
+
+global window, canvas, func_canvas, state
+state = {'month_selected':'2024'}
+DEPARTMENT = ['All','Orthopaedic Surgery','Otolaryngology','Gastroenterology']
+
+# MonthData = namedtuple(typename='MonthData', field_names=['month_txt', 'per_txt', 'rec_colour'])
+#
+# MONTH_DATA = {
+#     'Year': '2024',
+#     'Jan': MonthData('Jan', '30%', 'black'),
+#     'Feb': MonthData('Feb', '29%', 'black'),
+#     'Mar': MonthData('Mar', '28%', 'black'),
+#     'Apr': MonthData('Apr', '27%', 'black'),
+#     'May': MonthData('May', '26%', 'black'),
+#     'Jun': MonthData('Jun', '25%', 'black'),
+#     'Jul': MonthData('Jul', '24%', 'black'),
+#     'Aug': MonthData('Aug', '23%', 'black'),
+#     'Sep': MonthData('Sep', '22%', 'black'),
+#     'Oct': MonthData('Oct', '21%', 'black'),
+#     'Nov': MonthData('Nov', '20%', 'black'),
+#     'Dec': MonthData('Dec', '19%', 'black'),
+# }
+#
+
+# statistics = {f'{state["month_selected"]} YTD WTA': '30%',
+#               f'{state["month_selected"]} YTD Demand': '1000',
+#               f'{state["month_selected"]} YTD Supply': '120',
+#               f'{state["month_selected"]} Predicted WTA': '30%',
+#               f'{state["month_selected"]} Total Expected': '1200',
+#               f'{state["month_selected"]} Supply Adjustment': '130'
+#               }
+
+def get_all_data(*args):
+    print(f"Get All Data")
+def get_month_data_temp(*args):
+
+    selected_month = func_canvas.gettags('current')[0].replace("_btm","")
+    selected_month_idx = month2idx(selected_month)
+    clear_canvas_func()
+    create_main_screen(func_canvas, month=selected_month_idx)
+    # print(f"Get Month Data {func_canvas.gettags('current')[0]}")
+
+
+
+
+def create_month(x, y, month_txt, per_txt, rec_colour, SIZE_OF_WIDGET=(120, 110), outline='#000000', outline_width=1):
+    """
+    :param x: Left top position of X
+    :param y: Left top position of Y
+    :return: Frame
+    """
+
+    main_rec = func_canvas.create_rectangle(x, y, x + SIZE_OF_WIDGET[0], y + SIZE_OF_WIDGET[1], fill='#FFFFFF', width=outline_width, outline=outline, tags=f'{month_txt}_btm')
+    func_canvas.tag_bind(f'{month_txt}_btm', "<Button-1>", get_month_data_temp)
+
+    CENTER_X_OF_WIDGET = (x + SIZE_OF_WIDGET[0] / 2)
+    func_canvas.create_text(CENTER_X_OF_WIDGET, y + 17, anchor="center", text=month_txt, fill="#000000",
+                       font=("ArimoRoman Bold", 20 * -1))
+    PAD = 20
+    func_canvas.create_rectangle(x + PAD, y + 40, x + SIZE_OF_WIDGET[0] - PAD, y + 40 + 20, fill=rec_colour, outline='black')
+    func_canvas.create_text(CENTER_X_OF_WIDGET, y + 40 + 20 + 17, anchor='center', text=per_txt, fill='#000000',
+                       font=("ArimoRoman Bold", 20 * -1))
+
+# Rectangle for dropdown
+def create_month_grid(func_canvas, month_data, selected=0):
+    # START_GRID = (360, 220)
+    START_GRID = (60, 120)
+
+    SIZE_OF_WIDGET = (120, 110)
+    X_PAD = 10
+    Y_PAD = 20
+    CUR_XY = {'x': START_GRID[0], 'y': START_GRID[1]}
+
+    # Rectangle for monthly Grid
+    func_canvas.create_rectangle(
+        29.0,
+        81.0,
+        865.0,
+        551.0,
+        fill="",
+        outline="black")
+
+    YEAR_LOCATION = (413, 85)
+
+    func_canvas.create_text(
+        YEAR_LOCATION[0],
+        YEAR_LOCATION[1],
+        anchor="nw",
+        text=month_data['Year'],
+        fill="#000000",
+        font=("ArimoRoman Bold", 20 * -1)
+    )
+
+    button_image_4 = PhotoImage(
+        file=relative_to_assets("button_4.png"))
+    button_4 = Button(
+        func_canvas,
+        image=button_image_4,
+        borderwidth=1,
+        highlightthickness=0,
+        command=lambda: print("right button clicked"),
+        relief="flat",
+        background='white'
+    )
+    button_4.place(
+        x=470.0,
+        y=85.0,
+    )
+
+    button_image_5 = PhotoImage(
+        file=relative_to_assets("button_5.png"))
+    button_5 = Button(
+        func_canvas,
+        image=button_image_5,
+        borderwidth=1,
+        highlightthickness=0,
+        command=lambda: print("left button clicked"),
+        relief="flat",
+        background='white')
+    button_5.place(
+        x=368.0,
+        y=85.0,
+    )
+
+    for idx, (k, v) in enumerate(month_data.items()):
+        if k == 'Year':
+            continue
+        else:
+            if idx == selected:
+
+                outline = '#007d3e'
+                width = '3'
+            else:
+                outline = '#000000'
+                width = 1
+            CUR_XY.update(v._asdict())
+            create_month(outline_width=width, outline=outline,SIZE_OF_WIDGET=SIZE_OF_WIDGET, **(CUR_XY))
+            if k != 'Jun':
+                CUR_XY = {'x': CUR_XY['x'] + X_PAD + SIZE_OF_WIDGET[0], 'y': CUR_XY['y']}
+            else:
+                CUR_XY = {'x': START_GRID[0], 'y': START_GRID[1] + Y_PAD + SIZE_OF_WIDGET[1]}
+
+def create_statistic(func_canvas, statistic):
+    TITLE = (382, 380)
+    func_canvas.create_text(
+        *TITLE,
+        anchor="nw",
+        text="Statistics",
+        fill="#000000",
+        font=("ArimoRoman Bold", 20 * -1)
+    )
+    START_POS = (200, 420)
+    CUR_POS = START_POS
+
+    Y_PAD = 40
+    X_PAD = 400
+    SPACE = 150
+
+    # Rectangle for statistic
+    func_canvas.create_rectangle(
+        60.0,
+        372.0,
+        830.0,
+        535.0,
+        fill="",
+        outline="black")
+    statistic_d = {}
+    for idx, (k,v) in enumerate(statistic.items()):
+        key_var=tkinter.StringVar(func_canvas, value=k+":")
+
+
+        t_box = tkinter.Label(master=func_canvas, textvariable=key_var, bg='#edfff6',  font=("ArimoRoman Bold", 20 * -1,), justify='right')
+        func_canvas.create_window((CUR_POS[0], CUR_POS[1]), window=t_box)
+
+
+        v_var=tkinter.StringVar(func_canvas, value=v)
+        v_box = tkinter.Label(master=func_canvas, textvariable=v_var, bg="#edfff6", font=("ArimoRoman Bold", 20 * -1), justify='left', )
+
+        func_canvas.create_window((CUR_POS[0] + SPACE, CUR_POS[1]), window=v_box)
+
+        CUR_POS = (CUR_POS[0], CUR_POS[1] + Y_PAD)
+        if idx == 2:
+            CUR_POS = (START_POS[0] + X_PAD, START_POS[1])
+        statistic_d[idx] = {'k':key_var, 'v':v_var}
+    return statistic, statistic_d
+def create_main_screen(func_canvas, year=0, month=0, department=""):
+    global statistics, MONTH_DATA, statistics_d
+    from controller_db import get_all_months_data, refresh_database, refresh_config
+    refresh_database()
+    refresh_config()
+    selected_year = datetime.now().year if year == 0 else year
+    selected_month = datetime.now().month if month == 0 else month
+    department = 'ALL' if department=="" else department
+    MONTH_DATA, statistics = get_all_months_data(selected_year, selected_month, department)
+    REC = [30, 20, 865, 65]
+    func_canvas.create_rectangle(
+        *REC,
+        fill="#030C5D",
+        outline="black")
+    cur_select_department = tkinter.StringVar()
+    cur_select_department.set(department)
+    dropdown = tkinter.OptionMenu(func_canvas, cur_select_department, *DEPARTMENT, command=option_menu)
+    dropdown.configure(bg="#030C5D",fg='#FFFFFF', font=("OpenSansRoman Bold", 20 * -1), borderwidth=0, highlightthickness=0)
+    func_canvas.create_window((450,40), window=dropdown)
+    create_month_grid(func_canvas, MONTH_DATA, selected=selected_month)
+    statistics, statistics_d = create_statistic(func_canvas, statistics)
+
+
+if __name__ == "__main__":
+    global window, canvas, func_canvas
+    window, canvas, func_canvas, info = start_up()
+    side_buttons = start_side_button(window)
+    create_main_screen(func_canvas)
+    window.resizable(False, False)
+    window.mainloop()
