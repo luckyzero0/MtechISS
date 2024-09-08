Added Green Outline when selected Added Department Change Functionality Corrected Left Right images Added WTA calculation

Final Capstone/GUI/build/controller_model.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 
 from prophet import Prophet
-from pmdarima.arima import auto_arima
+
+
 def import_model():
     pass
 
@@ -12,75 +13,93 @@ def MAPE(test_data, predictions):
     test_data, predictions = np.array(test_data), np.array(predictions)
     return np.mean(np.abs((test_data - predictions) / test_data)) * 100
 
+
 def RMSE(test_data, predictions):
-  test_data, predictions = np.array(test_data), np.array(predictions)
-  return np.sqrt(np.mean((test_data - predictions)**2))
+    test_data, predictions = np.array(test_data), np.array(predictions)
+    return np.sqrt(np.mean((test_data - predictions) ** 2))
+
+
 # Model training and forecasting functions
-def train_arima(train_data, test_data):
+def train_predict_arima(train_data, test_data, evaluate=True):
+    from pmdarima.arima import auto_arima
     model = auto_arima(train_data)
     predictions = model.predict(n_periods=len(test_data))
-    mse = RMSE(test_data, predictions)
-    return predictions, mse
-
-def train_theta(train_data, test_data):
-    theta_constant = np.mean(train_data)
-    theta_predictions = [theta_constant] * len(test_data)
-    mse = RMSE(test_data, theta_predictions)
-    return theta_predictions, mse
-
-def default_model(data, predict_len):
-    return train_score_predict_prophet(data, predict_len)
+    if evaluate == True:
+        rmse = RMSE(test_data, predictions)
+        return predictions, rmse
+    else:
+        return predictions, None
 
 
+def train_predict_baseline(train_data, test_data, evaluate=True):
+    mean_value = np.mean(train_data)
+    predictions = [mean_value] * len(test_data)
+    if evaluate == True:
+        rmse = RMSE(test_data, predictions)
+        return predictions, rmse
+    else:
+        return predictions, None
 
-def train_score_predict_prophet(data, predict_len=3, model_parameters={}, model_type='prophet'):
+
+def train_predict_theta(train_data, test_data, evaluate=True):
+    from statsmodels.tsa.forecasting.theta import ThetaModel
+    model = ThetaModel(train_data, period=12)
+    res = model.fit()
+    predictions = res.forecast(len(test_data))
+    if evaluate == True:
+        rmse = RMSE(test_data, predictions)
+        return predictions, rmse
+    else:
+        return predictions, None
+
+
+def get_best_prediction(data, predict_len, config=None):
+    if config is None:
+        config = {'prophet': {'model_type': 'prophet'},
+                  'theata': {'model_type': 'theata'},
+                  'arima': {'model_type': 'arima'},
+                  'baseline': {'model_type': 'baseline'}}
+    output = config.copy()
+    for k, v in output.items():
+        v['predictions'], v['rmse'] = train_score_predict(data, predict_len, model_type=v.get('model_type', ''))
+        print(f"Completed {k} Model: {v['rmse']}")
+    best_model = min(output, key=lambda k: output[k]['rmse'])
+    print(f"Best Model: {best_model}")
+    print()
+    return output[best_model]['predictions'], output[best_model]['rmse']
+
+def train_score_predict(data, predict_len=3, model_parameters={}, model_type='prophet'):
     data = data.reset_index().rename(columns={'Months': 'ds', 'Demand': 'y'})
-    train_df = data[:-1*predict_len]
-    test_data = data[-1*predict_len:]
+    train_data = data[:-1 * predict_len]
+    test_data = data[-1 * predict_len:]
+    train_data_second = data[predict_len + 1:]
+    test_data_second = ['placeholder'] * predict_len
 
+    # Model Training
     if model_type == 'prophet':
         model = Prophet(**model_parameters)
-        model.fit(train_df)
+        model.fit(train_data)
         future = model.make_future_dataframe(periods=len(test_data), freq='MS')
         forecast = model.predict(future)
+        test_predictions = forecast['yhat'][-len(test_data):].values
+        rmse = RMSE(test_data['y'], test_predictions)
+        model = Prophet(**model_parameters)
+        model.fit(train_data_second)
+        future = model.make_future_dataframe(periods=predict_len, freq='MS')
+        forecast = model.predict(future)
+        predictions = forecast['yhat'][-predict_len:].values
 
-    elif model_type=='arima':
-        model = auto_arima(train_df)
-        forecast = model.predict(len(test_data))
+
+    elif model_type == 'arima':
+        _, rmse = train_predict_arima(train_data['y'], test_data['y'], evaluate=True)
+        predictions, _ = train_predict_arima(train_data_second['y'], test_data_second, evaluate=False)
 
     elif model_type == 'theata':
-        theata_constant = np.mean(train_df)
-        forecast = [theata_constant] * len(test_data)
+        _, rmse = train_predict_theta(train_data['y'], test_data['y'], evaluate=True)
+        predictions, _ = train_predict_arima(train_data_second['y'], test_data_second, evaluate=False)
 
-    test_predictions = forecast['yhat'][-len(test_data):].values
-    rmse = RMSE(test_data['y'], test_predictions)
+    elif model_type == 'baseline':
+        _, rmse = train_predict_baseline(train_data['y'], test_data['y'], evaluate=True)
+        predictions, _ = train_predict_arima(train_data_second['y'], test_data_second, evaluate=False)
 
-    if model_type == 'prophet':
-        model = Prophet(**model_parameters)
-
-    model.fit(pd.concat([data[predict_len+1:]]))
-    future = model.make_future_dataframe(periods=predict_len, freq='MS')
-    forecast = model.predict(future)
-    predictions = forecast['yhat'][-predict_len:].values
     return predictions, rmse
-
-
-def export_model():
-    pass
-
-
-def train_model():
-    pass
-    # with open(MODEL_PATH, 'wb') as f:
-    #     pickle.dump(model, f)
-    # pass
-
-
-
-def predict():
-
-    # with open(MODEL_PATH, 'rb') as f:
-    #     model = pickle.load(f)
-    # model.predict()
-
-    return {}